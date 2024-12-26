49482d6993272a7fe4fb59c8653193e105061867
Fuyuan Cheng <gloomcheng@gmail.com>
2024-12-26 15:06:43 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -16,5 +16,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
